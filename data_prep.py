d565d9a39829bcbffdf30f87f00592e392738d5f
4yxWBmlsIiQ40uQTml <848294@gmail.com>
2019-12-18 11:47:05 -0500

    Add data_prep.py

---
+++ data_prep.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Data preparation."""
+
+import os
+
+__author__ = 'Witold Ciolkiewicz'
+__copyright__ = 'Copyright 2019-present, Witold Ciolkiewicz'
+__credits__ = ['os: https://docs.python.org/3/']
+__license__ = 'CC-BY-NC-ND-4.0'
+__version__ = '{mayor}.{minor}.{rel}'
+__maintainer__ = 'Witold Ciolkiewicz'
+__email__ = 'd33pv3ct0r@gmail.com'
+__status__ = 'Dev'
